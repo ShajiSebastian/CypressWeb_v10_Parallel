f99e46e8ddb81129ab5d4825c668fa525cb8b040
Shaji Sebastian <star.shaji@gmail.com>
2023-02-22 14:15:09 +0000

    added

--- myShellScript.sh
+++
@@ -1,8 +0,0 @@
-echo "This is a shell script. This will be executed by runShellScript.yml file"
-echo "We can give any commands here"
-echo $RUNNER_WORKSPACE
-pwd
-ls -lrt
-java --version
-whoami
-env
